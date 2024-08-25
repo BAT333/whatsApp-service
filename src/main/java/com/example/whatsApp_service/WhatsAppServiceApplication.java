9d5eb01e9847c35c2e736784ee4ec211ad0fb7f1
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-08-25 13:53:15 -0300

    Class Configuration and default
    Class config exceptions

---
+++ src/main/java/com/example/whatsApp_service/WhatsAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsApp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whatsApp_service/WhatsAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsApp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
